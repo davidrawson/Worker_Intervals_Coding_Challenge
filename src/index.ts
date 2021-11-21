515647fc0ec61e0aeb8c3d76e303afaeae11a944
David Rawson <dm.rawson@gmail.com>
2021-11-21 22:19:20 +0000

    7 tests passed, still. A refactor and some pseudo code of next direction. A mess.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { start } from "repl";
 import { readInputFile, processInputFile } from "./utils/fileUtils";
 import {
   sortWorkerEarliestIntervals,
@@ -47,57 +48,61 @@ export async function solveThirdQuestion(
 ): Promise<string[]> {
   // TODO: Solve me!
 
-  // for each worker take an interval and see if another worker has an interval
-  // that overlaps. Work out times of the overlap and push to an array.
-  // Once the worker intervals have been checked for overlaps do the same
-  // for the array you have been pushing to.
-  // This is damn fiendish.
-
-  // Hack: Forget the worker - get all the intervals for all workers in one array
-  // and do the comarison with that
-
   const inputFile: string[] = await readInputFile(inputFilePath);
   const workers: Worker[] = processInputFile(inputFile);
 
   const allIntervals: Interval[] = getAllIntervals(workers);
   // this should really be of type Interval[]
-  const overlapIntervals: any = [];
+  const overlapIntervals: any = checkForOverlaps(allIntervals);
 
-  allIntervals.forEach((interval) => {
+  console.log("overlapIntervals ", overlapIntervals);
+
+  // here, I am trying to...remove the fragments.
+  overlapIntervals.forEach((interval: Interval, index: number) => {
     const start = interval.start;
     const end = interval.end;
+    const intervalIndex = index;
 
-    // another forEach to compare these start and end times
-    // looking for overlap
-    // push the overlap interval to a new array of intervals
-
-    allIntervals.forEach((interval) => {
-      const comparatorStart = interval.start;
-      const comparatorEnd = interval.end;
-
-      if (start < comparatorStart && end > comparatorStart) {
-        // we have an overlap from comparatorStart to
-        // the earliest of the ends
-        const overlapStart = new Date(comparatorStart).toISOString();
-        let overlapEnd = "";
-        console.log("overlapStart ", overlapStart);
-
-        if (end <= comparatorEnd) {
-          // comparatorStart to end
-          overlapEnd = new Date(end).toISOString();
-        } else {
-          overlapEnd = new Date(comparatorEnd).toISOString();
-        }
-        console.log("overlapEnd ", overlapEnd);
-        const overlapInterval: any = overlapStart + "/" + overlapEnd;
-        console.log("overlapInterval ", overlapInterval);
-
-        overlapIntervals.push(overlapInterval);
+    overlapIntervals.forEach((interval: Interval, index: number) => {
+      if (
+        interval.start >= start &&
+        interval.end <= end &&
+        intervalIndex != index
+      ) {
+        overlapIntervals.splice(index, 1);
       }
     });
   });
 
-  return overlapIntervals;
+  // *** This isn't working because checkForOverlaps is an
+  // exclusive check - it only uses the bit where there is overlap.
+  // I need inclusive - if there is overlap go from earliest to latest.
+
+  // const remainingOverlaps = checkForOverlaps(overlapIntervals);
+  // console.log("RemainingOverlaps ", remainingOverlaps);
+
+  // if (remainingOverlaps.length > 0) {
+  //   overlapIntervals.push(conjoinIntervals(remainingOverlaps));
+  // }
+
+  console.log("overlapIntervals2 ", overlapIntervals);
+
+  const isoIntervals: Interval[] = [];
+  overlapIntervals.forEach((interval: Interval) => {
+    const isoStart = new Date(interval.start).toISOString();
+    const isoEnd = new Date(interval.end).toISOString();
+    const isoInterval: any = isoStart + "/" + isoEnd;
+    isoIntervals.push(isoInterval);
+  });
+
+  console.log("isoIntervals", isoIntervals);
+
+  const uniqueIntervals: any[] = [...Array.from(new Set(isoIntervals))];
+
+  console.log("uniqueIntervals", uniqueIntervals);
+
+  // return overlapIntervals;
+  return uniqueIntervals;
 }
 
 const getAllIntervals = (workers: Worker[]) => {
@@ -108,5 +113,119 @@ const getAllIntervals = (workers: Worker[]) => {
     });
   });
 
+  const conjoinedIntervals = conjoinIntervals(intervals);
+  // conjoin consequecutive intervals
+  // intervals.forEach((interval: Interval, index: number) => {
+  //   const start = interval.start;
+  //   const end = interval.end;
+  //   const intervalIndex = index;
+
+  //   intervals.forEach((interval: Interval, index: number) => {
+  //     if (interval.start === end) {
+  //       const newInterval: any = { start: start, end: interval.end };
+
+  //       // intervals.splice(index, 1);
+  //       // intervals.splice(intervalIndex, 1);
+  //       intervals.push(newInterval);
+  //     }
+
+  //     if (start === interval.end) {
+  //       const newInterval: any = { start: interval.start, end: end };
+
+  //       // intervals.splice(index, 1);
+  //       // intervals.splice(intervalIndex, 1);
+  //       intervals.push(newInterval);
+  //     }
+  //   });
+  // });
+  console.log("conjoinedIntervals  ", conjoinedIntervals);
+
+  return conjoinedIntervals;
+};
+
+const checkForOverlaps = (allIntervals: any[]) => {
+  const overlapIntervals: any = [];
+
+  allIntervals.forEach((interval: Interval, index: number) => {
+    const start = interval.start;
+    const end = interval.end;
+    const intervalIndex = index;
+
+    console.log("Interval ", interval);
+
+    allIntervals.forEach((interval: Interval, index: number) => {
+      const comparatorStart = interval.start;
+      const comparatorEnd = interval.end;
+
+      if (
+        start < comparatorStart &&
+        end > comparatorStart &&
+        intervalIndex != index
+      ) {
+        // we have an overlap from comparatorStart to
+        // the earliest of the ends
+        let overlapInterval = {};
+
+        if (end <= comparatorEnd) {
+          // comparatorStart to end
+          overlapInterval = { start: comparatorStart, end: end };
+        } else {
+          // comparatorStart to comparatorEnd
+          overlapInterval = { start: comparatorStart, end: comparatorEnd };
+        }
+        // console.log("overlapInterval ", overlapInterval);
+
+        overlapIntervals.push(overlapInterval);
+      }
+
+      if (
+        start > comparatorStart &&
+        start < comparatorEnd &&
+        intervalIndex != index
+      ) {
+        // we have an overlap from start to the earliest of the ends
+        let overlapInterval = {};
+
+        if (end <= comparatorEnd) {
+          // start to end
+          overlapInterval = { start: start, end: end };
+        } else {
+          // start to comparatorEnd
+          overlapInterval = { start: start, end: comparatorEnd };
+        }
+        console.log("overlapInterval ", overlapInterval);
+
+        overlapIntervals.push(overlapInterval);
+      }
+    });
+  });
+
+  return overlapIntervals;
+};
+
+const conjoinIntervals = (intervals: any) => {
+  intervals.forEach((interval: Interval, index: number) => {
+    const start = interval.start;
+    const end = interval.end;
+    const intervalIndex = index;
+
+    intervals.forEach((interval: Interval, index: number) => {
+      if (interval.start === end) {
+        const newInterval: any = { start: start, end: interval.end };
+
+        // intervals.splice(index, 1);
+        // intervals.splice(intervalIndex, 1);
+        intervals.push(newInterval);
+      }
+
+      if (start === interval.end) {
+        const newInterval: any = { start: interval.start, end: end };
+
+        // intervals.splice(index, 1);
+        // intervals.splice(intervalIndex, 1);
+        intervals.push(newInterval);
+      }
+    });
+  });
   return intervals;
 };
